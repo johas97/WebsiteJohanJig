675400ff2bdb46baa2661bc9f3f1da851f968d71
Johan Jig <johan.jigsved@gmail.com>
2021-10-20 13:34:18 +0200

    Starten av projektet

---
+++ app4/web_site.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about/')
+def about():
+    return render_template('hellow.html')
+
+@app.route('/contact/')
+def contact():
+    return render_template('https://www.linkedin.com/in/johan-jigsved-540a26201/')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
