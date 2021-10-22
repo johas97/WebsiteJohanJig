ee3eededd4a2701bd07f87c08ebe0964920dd917
Johas97 <johan.jigsved@yahoo.se>
2021-10-22 13:37:23 +0200

    Continued on the contact form

--- app4/web_site.py
+++ app4/web_site.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, request
+import smtplib
 
 app = Flask(__name__)
 
@@ -7,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-"""
+
 @app.route('/mail_sent', methods=["POST"])
 def mail():
 
@@ -15,11 +16,24 @@ def mail():
     email = request.form.get("email")
     subject = request.form.get("subject")
     message = request.form.get("message")
-    if not name or email or subject or message:
+    print(name)
+    print(email)
+    print(subject)
+    print(message)
+    if not name or not email or not subject or not message:
         error_statment = "Enter all the information"
 
-    return render_template('mail.html')
-"""
+        return render_template('index.html', error_statment=error_statment)
+    else:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login("johas1997nov@gmail.com", "")
+        server.sendmail("johas1997nov@gmail.com", "johan.jigsved@yahoo.se", name + ": \n" + email +": \n" + subject + ": \n" + message)
+        return render_template('index.html', error_statment="Email sent!")
+
+
+
+
 
 
 
