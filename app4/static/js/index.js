dc4b292df1e52c01b5a0d85f0b76e18ccc2ae76d
Johas97 <johan.jigsved@yahoo.se>
2021-10-22 09:54:45 +0200

    Added a contact form, not done yet

--- app4/static/js/index.js
+++ app4/static/js/index.js
@@ -1,5 +1,35 @@
 const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
+
+navLinks.forEach(link => {
+    link.addEventListener('click', ()=> {
+        document.body.classList.remove('nav-open')
+    })
+})
+
+// Error handling mail form 
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const subject = document.getElementById('subject')
+const message = document.getElementById('message')
+const errorElement = document.getElementById('error')
+
+
+FormData.addEventListener('submit', (e) => {
+    let messages = []
+    if (name.value === '' || name.value == null){
+        messages.push('Name is required')
+        errorElement.innerText = messeges.join(', ')
+    }
+
+    if (messages.length > 0)
+    {
+        e.preventDefault()
+
+    }
+
+})
--- app4/web_site.py
+++ app4/web_site.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-import jyserver.Flask as jsf
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -8,15 +7,20 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+"""
+@app.route('/mail_sent', methods=["POST"])
+def mail():
 
+    name = request.form.get("name")
+    email = request.form.get("email")
+    subject = request.form.get("subject")
+    message = request.form.get("message")
+    if not name or email or subject or message:
+        error_statment = "Enter all the information"
 
-@app.route('/about/')
-def about():
-    return render_template('hellow.html')
+    return render_template('mail.html')
+"""
 
-@app.route('/contact/')
-def contact():
-    return render_template('https://www.linkedin.com/in/johan-jigsved-540a26201/')
 
 
 if __name__ == "__main__":
